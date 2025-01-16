539a88c48eb47000a43bd426186b9845237d9880
Sraka-Vurdalaka <158866385+Sraka-Vurdalaka@users.noreply.github.com>
2025-01-16 18:39:53 +0700

    Update script.js
    Добавлены скрипты для работы бургера

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function () {
     $('.slider-container').slick({
         arrows: true,
@@ -12,6 +13,7 @@ $(document).ready(function () {
     });
 });
 
+
 // модалка с видео начало
 let modalVideo = document.querySelector('.modal-video');
 let videoClose = document.querySelector('.video-close');
@@ -25,6 +27,7 @@ videoBtn.addEventListener('click', () => {
     modalVideo.classList.add('modal-video-active');
     modalVideo.classList.remove('modal-video-disactive');
     videoElement.play();
+
 });
 
 videoClose.addEventListener('click', () => {
@@ -34,3 +37,30 @@ videoClose.addEventListener('click', () => {
     videoElement.currentTime = 0;
 });
 // модалка с видео конец
+
+// Аккордион Start
+let accordeons = document.querySelectorAll('.accordion-item');
+accordeons.forEach(function (accord) {
+    accord.addEventListener('click', function (elem) {
+        let action = elem.target;
+        let currentAccordElem = action.closest('.accordion-item');//определяем текущий бокс
+        let currentContent = action.nextElementSibling; // находим скрытый контент
+        currentAccordElem.classList.toggle("active"); //присваиваем основному контейнеру флаг активности   
+        if (currentAccordElem.classList.contains("active")) {
+            currentContent.style.maxHeight = currentContent.scrollHeight + "px";
+            //Обратить внимание чтоб стили были присвоены классу .accordion-collapse
+        }
+        else {
+            currentContent.style.maxHeight = 0;
+        }
+    });
+});
+// Аккордион End
+
+let menu = document.querySelector('.menu-mobile');
+let menuList = document.querySelector('.menu-mobile-list');
+
+menu.addEventListener('click', function () {
+    menuList.classList.toggle('mobile-active');
+
+});
